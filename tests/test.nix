534ee577692c7092fdcd035f89bc29b663c6f9ca
Skye <me@skye-c.at>
2025-03-23 14:29:17 -0400

    Remove `test.pristine`, generating it dynamically from reference instead.

--- tests/test.nix
+++ tests/test.nix
@@ -1,16 +1,9 @@
 {
   pkgs,
   reference ? false,
-  ...
-}:
+}@args:
+
 let
-  generate-testscript = pkgs.writeText "generate-testscript" ''
-    machine.wait_for_unit("default.target")
-    machine.succeed("${before-script}")
-    machine.succeed("${tmpfiles} --create ${test-config}")
-    machine.succeed("bash ${print-script} > output")
-    machine.copy_from_vm(source="output")
-  '';
   before-script = pkgs.writeShellScript "before-script.sh" ''
     mkdir /root/existing-dir
   '';
@@ -28,40 +21,64 @@ let
     ls -FloRA --time-style=+"" /root
     find /root -print0 | sort --zero-terminated | xargs --null lsattr -d || true
   '';
-  pristine-file = pkgs.writeText "test.pristine" (builtins.readFile ./test.pristine);
-  tmpfiles = if reference then "systemd-tmpfiles" else "mini-tmpfiles";
-  test = pkgs.testers.runNixOSTest {
-    name = if reference then "reference-test" else "mini-test";
-    nodes.machine =
-      {
-        config,
-        pkgs,
-        lib,
-        ...
-      }:
-      {
-        boot.loader.systemd-boot.enable = true;
-        environment.systemPackages = lib.optionals (!reference) [
-          pkgs.mini-tmpfiles
-        ];
-        # Once mini-tmpfiles is supports everything used during booting we can replace systemd-tmpfiles with it
-        # systemd.services.systemd-tmpfiles-setup.serviceConfig = {
-        #   ExecStart = [
-        #     ""
-        #     "${pkgs.mini-tmpfiles}/bin/mini-tmpfiles --create --remove --boot --exclude-prefix=/dev --exclude-prefix=/sysroot"
-        #   ];
-        # };
+  testFile = (
+    {
+      generating ? false,
+    }:
+    let
+      generate-output = ''
+        machine.wait_for_unit("default.target")
+        machine.succeed("${before-script}")
+        machine.succeed("${tmpfiles} --create ${test-config}")
+        machine.succeed("bash ${print-script} > output")
+      '';
+      tmpfiles = if reference || generating then "systemd-tmpfiles" else "mini-tmpfiles";
+      generate-testscript = pkgs.writeText "generate-testscript" (
+        generate-output
+        + ''
+          machine.copy_from_vm(source="output")
+        ''
+      );
+      generated = (testFile { generating = true; }).generate;
+      test = pkgs.testers.runNixOSTest {
+        name = if reference then "reference-test" else "mini-test";
+        nodes.machine =
+          {
+            config,
+            pkgs,
+            lib,
+            ...
+          }:
+          {
+            boot.loader.systemd-boot.enable = true;
+            environment.systemPackages = lib.optionals (!(reference || generating)) [
+              pkgs.mini-tmpfiles
+            ];
+            # Once mini-tmpfiles is supports everything used during booting we can replace systemd-tmpfiles with it
+            # systemd.services.systemd-tmpfiles-setup.serviceConfig = {
+            #   ExecStart = [
+            #     ""
+            #     "${pkgs.mini-tmpfiles}/bin/mini-tmpfiles --create --remove --boot --exclude-prefix=/dev --exclude-prefix=/sysroot"
+            #   ];
+            # };
+          };
+        testScript = pkgs.lib.optionalString (!generating) (
+          generate-output
+          + ''
+            machine.succeed("diff ${generated} output")
+          ''
+        );
+        passthru.generate =
+          if generating then
+            pkgs.runCommand "generated" { } ''
+              ${test.driver}/bin/nixos-test-driver ${generate-testscript}
+              mv output $out
+            ''
+          else
+            generated;
       };
-    testScript = ''
-      machine.wait_for_unit("default.target")
-      machine.succeed("${before-script}")
-      machine.succeed("${tmpfiles} --create ${test-config}")
-      machine.succeed("bash ${print-script} | diff ${pristine-file} -")
-    '';
-    passthru.generate = pkgs.runCommand "generated" { } ''
-      ${test.driver}/bin/nixos-test-driver ${generate-testscript}
-      mv output $out
-    '';
-  };
+    in
+    test
+  );
 in
-test
+testFile { }
